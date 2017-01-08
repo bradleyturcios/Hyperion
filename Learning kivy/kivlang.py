cd4c2aab40ab79506893b40e37c3098087754da0
Bradley Turcios <bradleyturcios@ymail.com>
2017-01-08 11:52:46 -0800

    Fixed/bugs started new project
    fixed the class name syntax of the "learning kivy" problem, giving a
    black screen. Started a new project camera that captures video through
    webcam. Start of application that will use crossbar to send video

--- Learning kivy/kivlang.py
+++ Learning kivy/kivlang.py
@@ -10,19 +10,19 @@ from kivy.uix.widget import Widget      #Give ability to understand that you are
                                             so someone else has written code for this before, and compiled it into libraries. We just "import" it and manipulate it to our need"""
 
 
-class titlewidget(Widget):          #Creating a class, called titlewidget, that will hold the two labels (textboxes) of the title
+class TitleWidget(Widget):          #Creating a class, called titlewidget, that will hold the two labels (textboxes) of the title
     pass                            #"Pass" just means do nothing and continue
 
-class textwidget(Widget):           #Creating a class, called textwidget, that will hold text, that is the sensor values.
+class TextWidget(Widget):           #Creating a class, called textwidget, that will hold text, that is the sensor values.
     pass                            #Same pass as before
 
-class mainwidget(Widget):            #Create a class that will be the main widget. This is where all the parts and widgets will be condensed into one window.
+class MainWidget(Widget):            #Create a class that will be the main widget. This is where all the parts and widgets will be condensed into one window.
     pass                            #Same pass paas before
 
 class MC(App):                      #This is the class we need to create for python. We inherit from App. That is, we tell this class to expect to use all of the components of an application.
     def build(self):                #Create the build function. This is function that will hold all of the rules for how the final application will look.
         self.load_kv('mc.kv')       #We tell the file to look at the mc.kv file.
-        return mainwidget()         #We then tell it which class is our main class, that is the one with all of th pieces condensed into one window. And we return it, thas is run it.
+        return Mainwidget()         #We then tell it which class is our main class, that is the one with all of th pieces condensed into one window. And we return it, thas is run it.
 
 if __name__=="__main__":            #Some syntax stuff idk what this does, just know you need it for running object oriented programs.
     MC().run()                      # We tell python which one class is the one to run. In this case, the MC class is the one that builds the kivy application. That is we run mc, which knows how to display application,
