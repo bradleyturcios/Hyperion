096a0acd7e114ade2371102b06d27c10129016ac
Bradley Turcios <bradleyturcios@ymail.com>
2017-01-11 13:02:33 -0800

    change some stuff
    just some priv and pub keys

--- Learning Crossbar/cross.py
+++ Learning Crossbar/cross.py
@@ -1,7 +1,8 @@
-rom autobahn.twisted.wamp import ApplicationSession
+from autobahn.twisted.wamp import ApplicationSession
 from autobahn.twisted.util import sleep
 from twisted.internet.defer import inlineCallbacks
-
+from PIL import Image
+    
 class MyComponent(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
@@ -12,3 +13,5 @@ class MyComponent(ApplicationSession):
             self.publish(u'com.myapp.oncounter', counter)
             counter += 1
             yield sleep(1)
+
+
